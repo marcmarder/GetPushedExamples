0ee7edcf321241c8454d2219f1d658b06c9054d2
Marc Wiederholt <24612516+marcmarder@users.noreply.github.com>
2023-04-29 15:01:55 +0200

    feat: added bash

---
+++ bash/notification.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+send_notification() {
+  local channel_id="$1"
+  local notification_title="$2"
+  local notification_body="$3"
+
+  local base_url="https://us-central1-getpushed-8ad1d.cloudfunctions.net/createNotificationEndpoint/${channel_id}"
+
+  # Make http post
+  local result=$(curl -s -w "\n%{http_code}" -X POST "${base_url}" \
+    -H "Content-Type: application/json" \
+    -d "{\"title\": \"${notification_title}\", \"description\": \"${notification_body}\"}")
+
+  local response_body=$(echo "${result}" | head -n1)
+  local status_code=$(echo "${result}" | tail -n1)
+
+  echo "Notification Result: ${status_code} ${response_body}"
+}
+
+send_notification "channel_id" "notification_title" "notification_body"
