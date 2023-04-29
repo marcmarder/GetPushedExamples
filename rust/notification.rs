9a423d10262897ab3f13964d2e833e32f493aeaa
Marc Wiederholt <24612516+marcmarder@users.noreply.github.com>
2023-04-29 15:21:40 +0200

    feat: added rust

---
+++ rust/notification.rs
@@ -0,0 +1,42 @@
+use reqwest::header::{HeaderMap, CONTENT_TYPE};
+use serde_json::json;
+use std::error::Error;
+
+async fn send_notification(
+    channel_id: &str,
+    notification_title: &str,
+    notification_body: &str,
+) -> Result<(), Box<dyn Error>> {
+    let base_url = format!(
+        "https://us-central1-getpushed-8ad1d.cloudfunctions.net/createNotificationEndpoint/{}",
+        channel_id
+    );
+
+    // Make http post
+    let client = reqwest::Client::new();
+    let mut headers = HeaderMap::new();
+    headers.insert(CONTENT_TYPE, "application/json".parse().unwrap());
+
+    let data = json!({
+        "title": notification_title,
+        "description": notification_body,
+    });
+
+    let response = client.post(&base_url).headers(headers).json(&data).send().await?;
+
+    println!(
+        "Notification Result: {} {}",
+        response.status(),
+        response.text().await?
+    );
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    match send_notification("channel_id", "notification_title", "notification_body").await {
+        Ok(_) => println!("Notification sent successfully"),
+        Err(e) => println!("Error: {}", e),
+    }
+}
